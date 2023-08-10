22d1d25e708da2436a3600d00247d8c32941cd37
Antonio Rufino <devs.toni@gmail.com>
2023-08-10 20:28:39 +0200

    feat: add countries endpoint

--- app.py
+++ app.py
@@ -5,5 +5,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+@app.route('/api/countries')
+def getAllCountries():
+    return "Hola País!"
+
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
