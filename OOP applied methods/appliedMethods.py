e57053988b87b74adda1ec2b1fd906faa1b28d46
QianyingLuo <qianyingluodev@gmail.com>
2023-10-31 21:24:27 +0100

    feeat: delete submodule

---
+++ OOP applied methods/appliedMethods.py
@@ -0,0 +1,21 @@
+texto = input("Introduzca un texto: ")
+isAllText = texto.isalpha()
+isAllDigits = texto.isdigit()
+isCharactersAndNumbers = texto.isalnum()
+print(isAllText)
+print(isAllDigits)
+print(isCharactersAndNumbers)
+
+texto2 = input("Introduzca un texto: ")
+posicion = texto2.find("mundo") 
+if posicion != -1:
+    resultado = texto2[posicion:]
+    print(resultado)
+
+print("-" *20)
+print("Partiendo de una frase con diferentes palabras separadas por espacios, crear una lista donde cada elemento sea cada palabra del texto. Mostrar dicha lista y mostrar el número de veces que se encuentra la palabra mundo en esa lista")
+texto3 = "Hola mundo mundo adios"
+print("Texto=", texto3)
+lista = texto3.split() 
+print(lista)
+print("Número de veces que aparece la palabra mundo:", texto3.count("mundo"))
